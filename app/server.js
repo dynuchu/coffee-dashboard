40620e6c19287ad3a8b7bc51e5a786bb75761dd5
Agnes Bosskay <dynuchu@gmail.com>
2026-09-06 19:50:43 +0200

    Complete coffee CRUD API

--- app/server.js
+++ app/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 
 const app = express();
 const port = 3000;
@@ -38,6 +38,45 @@ app.post("/api/coffees", async (req, res) => {
   } catch (error) {
     console.error("Failed to add coffee: ", error);
     res.status(500).json({ error: "Failed to add coffee"});
+  }
+});
+
+app.put("/api/coffees/:id", async (req, res) => {
+  try {
+    const db = client.db("coffee-dashboard");
+
+    const result = await db.collection("coffees").updateOne(
+      { _id: new ObjectId(req.params.id) },
+      { $set: req.body }
+    );
+
+    if (result.matchedCount === 0) {
+      return res.status(404).json({ error: "Coffee not found" });
+    }
+
+    res.json({ message: "Coffee updated" });
+  } catch (error) {
+    console.error("Failed to update coffee:", error);
+    res.status(500).json({ error: "Failed to update coffee" });
+  }
+});
+
+app.delete("/api/coffees/:id", async (req, res) => {
+  try {
+    const db = client.db("coffee-dashboard");
+
+    const result = await db.collection("coffees").deleteOne({
+      _id: new ObjectId(req.params.id)
+    });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ error: "Coffee not found "});
+    }
+
+    res.json({message: "Coffee deleted"});
+  } catch (error) {
+    console.error("Failed to delete coffee:", error);
+    res.status(500).json({error: "Failed to delete coffee"});
   }
 });
 
