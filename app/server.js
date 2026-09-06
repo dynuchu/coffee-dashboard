f2b76e0c1fa5ef8d1acff5e772fbd72a56fb9b57
Agnes Bosskay <dynuchu@gmail.com>
2026-09-06 19:28:16 +0200

    Add MongoDB coffee API

--- app/server.js
+++ app/server.js
@@ -1,13 +1,44 @@
-const http = require("http");
+const express = require("express");
 const { MongoClient } = require("mongodb");
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
 
 const client = new MongoClient(
   `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@mongodb:27017/?authSource=admin`
 );
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-  res.end("☕ Hello from my Dockerized Coffee Dashboard!");
+app.get("/", (req, res) => {
+  res.send("☕ Hello from my Dockerized Coffee Dashboard!");
+});
+
+app.get("/api/coffees", async (req, res) => {
+  try {
+    const db = client.db("coffee-dashboard");
+    const coffees = await db.collection("coffees").find().toArray();
+
+    res.json(coffees);
+  } catch (error) {
+    console.error("Failed to fetch coffees:", error);
+    res.status(500).json({ error: "Failed to fetch coffees" });
+  }
+});
+
+app.post("/api/coffees", async (req, res) => {
+  try {
+    const db = client.db("coffee-dashboard");
+    const result = await db.collection("coffees").insertOne(req.body);
+
+    res.status(201).json({
+      message: "Coffee added",
+      coffeeId: result.insertedId
+    });
+  } catch (error) {
+    console.error("Failed to add coffee: ", error);
+    res.status(500).json({ error: "Failed to add coffee"});
+  }
 });
 
 async function start() {
@@ -15,8 +46,8 @@ async function start() {
     await client.connect();
     console.log("Connected to MongoDB!");
 
-    server.listen(3000, () => {
-      console.log("Coffee Dashboard is running on port 3000");
+    app.listen(port, () => {
+      console.log(`Coffee Dashboard is running on port ${port}`);
     });
   } catch (error) {
     console.error("MongoDB connection failed:", error);
